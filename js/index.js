7cc1cf60608d4fb5899b0e024c8ca226672f7fce
Cecily Lo <cecily.lo95@gmail.com>
2016-07-28 15:54:04 -0400

    animated scrolling

--- js/index.js
+++ js/index.js
@@ -32,7 +32,18 @@ $(document).ready(function() {
 
 
 
-
+ $(".scroll a").click(function(event){
+         event.preventDefault();
+         //calculate destination place
+         var dest=0;
+         if($(this.hash).offset().top > $(document).height()-$(window).height()){
+              dest=$(document).height()-$(window).height();
+         }else{
+              dest=$(this.hash).offset().top;
+         }
+         //go to destination
+         $('html,body').animate({scrollTop:dest}, 1000,'swing');
+ });
 
 /*
 function renderNight() {
