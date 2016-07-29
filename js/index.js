97ab1844a4c512568334515c24453b23f1ee9def
Cecily Lo <cecily.lo95@gmail.com>
2016-07-29 03:05:55 -0400

    mobile scrolling animation

--- js/index.js
+++ js/index.js
@@ -34,15 +34,14 @@ $(document).ready(function() {
 
  $(".scroll a").click(function(event){
          event.preventDefault();
-         //calculate destination place
-         var dest=0;
-         if($(this.hash).offset().top > $(document).height()-$(window).height()){
-              dest=$(document).height()-$(window).height();
-         }else{
-              dest=$(this.hash).offset().top;
+         var dest = 0;
+         if($(this.hash).offset().top > $(document).height() - $(window).height()){
+              dest = $(document).height() - $(window).height();
+         } else{
+              dest = $(this.hash).offset().top;
          }
-         //go to destination
-         $('html,body').animate({scrollTop:dest}, 1000,'swing');
+     
+         $('html, body').animate({scrollTop:dest}, 1000,'swing');
  });
 
 /*
